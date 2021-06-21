f6f2450d8a6d4a6b12107f6134da6a52e2d3776e
lgerardlucas <lgerardlucas@gmail.com>
2021-06-21 15:25:59 -0300

    Included icones in projects menu system

--- furniture/views.py
+++ furniture/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def list_furniture(request):
     context = {
-        'title':'Lista de Móveis',
+        'title':'Cadastro de Móveis',
         'about':'Cadastre aqui: Roupeiro, Cozinha, etc...',
     }
     return render(request,'list_furniture.html',context)
